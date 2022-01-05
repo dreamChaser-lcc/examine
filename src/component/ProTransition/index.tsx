d2586a0a1a16adb9473ef916d98270711b32aacd
lifachao <2390210930@qq.com>
2022-01-05 16:49:00 +0800

    :bug: fix:动画组件更新

--- src/component/ProTransition/index.tsx
+++ src/component/ProTransition/index.tsx
@@ -1,48 +1,36 @@
-import { FC } from 'react';
+import { FC, Key } from 'react';
 // 组件
 import {
-  TransitionGroup,
-  Transition,
   CSSTransition,
+  SwitchTransition,
 } from 'react-transition-group';
 // 样式
 import './style.less';
 
 interface IProTransition {
-  disabled: boolean;
+  animatekey?: Key;
+  mode?: 'out-in' | 'in-out';
 }
 /**过渡动画组件 */
 const ProTransition: FC<IProTransition> = (props) => {
-  const { children, disabled } = props;
-  const defaultStyle = {
-    transition: `opacity ${500}ms ease-in-out`,
-    opacity: 0,
-  };
-
-  const transitionStyles: any = {
-    entering: { opacity: 1 },
-    entered: { opacity: 1 },
-    exiting: { opacity: 0 },
-    exited: { opacity: 0 },
-  };
-  return (
+  const { children, mode, animatekey } = props;
+  const cssMode = (
     <div className="protransition-wrapper">
-      <Transition in={disabled} timeout={500}>
-        {(state: any) => (
-          <div
-            style={{
-              ...defaultStyle,
-              ...transitionStyles[state],
-            }}
-          >
-            {children}
-          </div>
-        )}
-        {/* <CSSTransition key={'css-transition'} timeout={300} classNames="item">
-       
-        </CSSTransition> */}
-      </Transition>
+      <SwitchTransition mode={mode}>
+        <CSSTransition
+          key={animatekey}
+          timeout={500}
+          unmountOnExit
+          classNames="fade"
+        >
+          {children}
+        </CSSTransition>
+      </SwitchTransition>
     </div>
   );
+  return cssMode;
+};
+ProTransition.defaultProps = {
+  mode: 'out-in',
 };
 export default ProTransition;
