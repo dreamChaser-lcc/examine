478a7d610149a71def802c3937313bf2ddc36089
dreamChaser-lcc <2390210930@qq.com>
2022-02-28 22:27:32 +0800

    :lipstick: style:dashboard样式

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,3 +1,35 @@
-export default()=>{
-  return <>首页</>
-}
+import { useMemo } from 'react';
+// 方法
+import moment from 'moment';
+// 样式
+import './style.less';
+
+export default () => {
+  const curDate = useMemo(() => {
+    const cur = new Date().getTime();
+    return moment(cur).format('YYYY-MM-DD HH:mm:ss');
+  }, []);
+  return (
+    <>
+      <div className="dashboard-wrap">
+        <header>
+          <h1>Welcome to my space,Lcc</h1>
+          <h5>{curDate}</h5>
+        </header>
+        <div className="container">
+          <div className="first-row">
+            <div className="left">1</div>
+
+            <div className="center">2</div>
+
+            <div className="right">3</div>
+          </div>
+          <div className="second-row">
+            <div className="left"></div>
+            <div className="right"></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
