4c252ec47adfaa3ab597aa14c302e6bcf71edfd4
lcc <2390210930@qq.com>
2023-05-21 21:21:20 +0800

    git commit -m ":sparkles:feat: 打包优化"

--- .umirc.ts
+++ .umirc.ts
@@ -12,6 +12,66 @@ const deployConfig = {
   // logo: `${DEPLOY_BASE_URL}logo.png`,
   favicon: `${DEPLOY_BASE_URL}favicon.ico`,
   publicPath: DEPLOY_BASE_URL,
+  chunks: ['antdesigns', 'echarts', 'vendors', 'umi'],
+  // 打包分析配置
+  analyze: {
+    analyzerMode: 'server',
+    analyzerPort: 8888,
+    openAnalyzer: true,
+    // generate stats file while ANALYZE_DUMP exist
+    generateStatsFile: false,
+    statsFilename: 'stats.json',
+    logLevel: 'info',
+    defaultSizes: 'parsed',
+  },
+  chainWebpack(config: any) {
+    if (isProduction) {
+      config.output.filename('[name].[hash:8].js');
+    }
+    config.merge({
+      optimization: {
+        minimize: true,
+        splitChunks: {
+          chunks: 'async',
+          minSize: 30000,
+          minChunks: 1,
+          automaticNameDelimiter: '.',
+          cacheGroups: {
+            antdesigns: {
+              name: 'antdesigns',
+              chunks: 'all',
+              test: /[\\/]node_modules[\\/](@antv|antd|@ant-design)/,
+              priority: 11,
+              enforce: true,
+            },
+            eChart: {
+              name: 'echarts',
+              chunks: 'all',
+              test: /[\\/]node_modules[\\/](echarts|echarts-for-react)/,
+              priority: 10,
+              enforce: true,
+            },
+            vendor: {
+              name: 'vendors',
+              chunks: 'all',
+              test: /[\\/]node_modules[\\/](lodash|moment)/,
+              priority: 9,
+              enforce: true,
+            },
+          },
+        },
+      },
+    });
+  },
+  externals: {
+    react: 'window.React',
+    'react-dom': 'window.ReactDom',
+  },
+  scripts: [
+    'https://unpkg.com/react@17/umd/react.production.min.js',
+    'https://unpkg.com/react-dom@17/umd/react-dom.production.min.js',
+  ],
+  hash: true, // hash文件,可以避免部署后前文件被浏览器缓存未刷新
 };
 
 export default defineConfig({
@@ -38,17 +98,17 @@ export default defineConfig({
   // mfsu:{},
   plugins: ['./src/plugins/plugin-keep-alive/src/index.js'],
   history: { type: 'hash' },
-  chainWebpack(config) {
-    if (isProduction) {
-      config.output.filename('[name].[hash:8].js');
-    }
-  },
+  // chainWebpack(config) {
+  //   if (isProduction) {
+  //     config.output.filename('[name].[hash:8].js');
+  //   }
+  // },
   // hash: true,	// 清除缓存
   base: '/',
   ...deployConfig,
   proxy: {
     '/api': {
-      target: 'http://150.158.189.25:8006',
+      target: 'http://licc.cloud:8006',
       changeOrigin: true,
       pathRewrite: { '^/api': '' },
     },
